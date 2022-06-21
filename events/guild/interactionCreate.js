eb95ee5d84f47b88ba8482ef049b2aba74ac0ce8
franciscososanet <fransosa.dev@gmail.com>
2022-06-21 20:38:48 -0300

    ticket, boton ej, menu ej, pregunta

--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -2,6 +2,36 @@ const discord = require("discord.js");
 
 module.exports = async (client, discord, interaction)  => {
 
+    //CREAR TICKET
+    if(interaction.isButton()){
+        if(interaction.customId === "tickets"){
+            const everyone = interaction.guild.roles.cache.find(r => r.name === "@everyone");
+            interaction.guild.channels.create(`ticket-${interaction.user.username}`, {
+                type: "GUILD_TEXT",
+                parent: "988508142381965373",
+                permissionOverwrites: [
+                    {
+                        id: interaction.user.id,
+                        allow: ["VIEW_CHANNEL", "SEND_MESSAGES"]
+                    },
+                    {
+                        id: everyone.id,
+                        deny: ["VIEW_CHANNEL", "SEND_MESSAGES"]
+                    }
+                ]
+            }).then(c => {
+                const mensaje = new discord.MessageEmbed()
+                    .setTitle(`${interaction.user.username}, este es tu ticket`)
+                    .setDescription("Escribe tu duda/pregunta/queja.\nSé paciente y espera por un moderador.")
+                    .setColor("RANDOM");
+
+                c.send({ embeds: [mensaje] });
+            });    
+
+            interaction.reply({ content: `<@${interaction.user.id}>, tu ticket ha sido creado.`, ephemeral: true });
+        }
+    }
+
     //ejecucion de comandos y context menu
     if(interaction.isCommand() || interaction.isContextMenu()){
         const command = client.slash.get(interaction.commandName);
